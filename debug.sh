439a65ddaeb65f91339f57fe6ff400791e9fbdb6
Hunter Ashton <hbashton13@gmail.com>
2017-03-26 19:56:00 -0500

    Remove patch from auto-applying

---
+++ debug.sh
@@ -0,0 +1,2 @@
+cd MadelineProto
+git am -3 < ../patches/0001-Remove-log-spam.patch
--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git submodule update
 composer update
-cd MadelineProto
-git am -3 < ../patches/0001-Remove-log-spam.patch
+. start.sh
