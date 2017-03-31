e3165321445bf865363130598b8e7c90cdfcc805
Hunter Ashton <hbashton13@gmail.com>
2017-03-31 15:41:24 -0500

    update update script

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 git submodule update
 [ $(git remote | grep -c BruhhBot-Madeline) -eq 0 ] && git remote add BruhhBot-Madeline https://github.com/hbashton/BruhhBot-Madeline.git
-git fetch BruhhBot-Madeline && git checkout BruhhBot-Madeline/master
+git fetch BruhhBot-Madeline && git pull BruhhBot-Madeline master
