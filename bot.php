c2e70aebfec9f9b0bff70441e7a4004bec778009
Hunter Ashton <hbashton13@gmail.com>
2017-04-09 22:55:47 -0500

    Fix MTPROTO_SETTINGS

--- bot.php
+++ bot.php
@@ -57,10 +57,7 @@ if (isset($argv[1])) {
 } else {
     $dumpme = false;
 }
-
-$MTPROTO_SETTINGS={"app_info":{"api_id":6,"api_hash":"eb06d4abfb49dc3eeb1aeb98ae0f581e"},"connection_settings":{"all":{"test_mode":true}}}
-
-$settings = json_decode($MTPROTO_SETTINGS), true) ?: [];
+$settings = [];
 
 if (!isset($MadelineProto)) {
     $MadelineProto = new \danog\MadelineProto\API($settings);
