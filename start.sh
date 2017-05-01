88e5355a486ed260125b99a48275b746379c6291
Hunter Ashton <hbashton13@gmail.com>
2017-05-01 01:24:02 -0500

    bugfix

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 until php bot.php; do
     echo "The bot crashed with exit code $?.  Respawning.." >&2
-    rm bot.madeline
+    [ $(ls | grep -c bot.madeline) -eq 1 ] && rm bot.madeline
     sleep 2
 done
