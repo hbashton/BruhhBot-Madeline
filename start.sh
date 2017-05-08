df43e0eff78f495ad7d999f9cb5aa62a8dad48bf
Hunter Ashton <hbashton13@gmail.com>
2017-05-07 22:38:26 -0500

    keep the bot alive forever :D

--- start.sh
+++ start.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
-
-until php bot.php; do
-    echo "The bot crashed with exit code $?.  Respawning.." >&2
-    [ $(ls | grep -c bot.madeline) -eq 1 ] && rm bot.madeline
-    sleep 2
+function clean_up {
+    if [ ! -f bot.pid ]; then
+        kill -9 $pid
+    else
+        kill -9 `cat bot.pid`
+    fi
+    echo "Stopping the bot and removing the .pid file"
+    exit
+}
+trap clean_up SIGHUP SIGINT SIGTERM
+php bot.php & export pid=$!
+echo -n "$pid" > bot.pid
+echo "The PID of the bot is $pid. If you run this script in the background, to stop it, just remove the bot.pid file created in this folder"
+elapsed=0
+while true; do
+  if [ "$elapsed" -gt "3600" ]; then
+    elapsed=0
+    if [ -f "bot.pid" ]; then
+      kill -9 $(cat bot.pid)
+      rm bot.pid
+    fi
+    php bot.php & export pid=$!
+    echo -n "$pid" > bot.pid
+  fi
+  if [ ! -f "bot.pid" ]; then
+    echo "No PID file found"
+    if [ ! -z "$pid" ]; then
+      kill -9 "$pid"
+      exit
+    fi
+  fi
+  alive=$(ps aux | grep "$pid" | grep -v "grep" | wc -l)
+  if [ "$alive" == "0" ]; then
+    echo "Bot dead. Reviving"
+    php bot.php & export pid=$!
+    echo -n "$pid" > bot.pid
+  fi
+  sleep 1
+  elapsed=$((elapsed+1))
 done
+clean_up
