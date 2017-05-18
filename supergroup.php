4912c02f63dbcbdb0232ed1f1c181b3680a4a6c3
Hunter Ashton <hbashton13@gmail.com>
2017-05-18 08:45:55 -0500

    bugfix

--- supergroup.php
+++ supergroup.php
@@ -721,7 +721,7 @@ function get_chat_rules($update, $MadelineProto)
                     );
                 } catch (Exception $e) {
                     var_dump($e->getMessage());
-                    if ($e->getMessage() == "USER_IS_BLOCKED" or $e->getMessage() == "PEER_ID_INVALID") {
+                    if ($e->getMessage() == "USER_IS_BLOCKED" or $e->getMessage() == "PEER_ID_INVALID" or $e->getMessage() == "The provided peer id is invalid") {
                         if (isset($default['entities'])) unset($default['entities']);
                         $default['peer'] = $peer;
                         $botusername = preg_replace("/@/", "",getenv("BOT_API_USERNAME"));
