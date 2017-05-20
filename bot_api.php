dee51844e6a60f700067e221ed2af06d847b6f4c
Hunter Ashton <hbashton13@gmail.com>
2017-05-19 22:10:14 -0500

    Remove some exception catching from way earlier

--- bot_api.php
+++ bot_api.php
@@ -11,23 +11,21 @@ function bot_present($update, $MadelineProto, $silent = false, $peer = false, $u
             $diff = time() - $MadelineProto->API->is_bot_present[$peer]["timestamp"];
             if ($diff < 600) {
                 if (!$MadelineProto->API->is_bot_present[$peer]["return"]) {
-                    try {
-                        if (!$silent) {
-                            $peer = $MadelineProto->get_info($update['update']['message']['to_id'])
-                            ['InputPeer'];
-                            $msg_id = $update['update']['message']['id'];
-                            $str = $MadelineProto->responses['bot_present']['not'];
-                            $repl = array(
-                                "botname" => getenv('BOT_USERNAME')
-                            );
-                            $message = $MadelineProto->engine->render($str, $repl);
-                            $sentMessage = $MadelineProto->messages->sendMessage(
-                                ['peer' => $peer, 'reply_to_msg_id' =>
-                                $msg_id, 'message' => $message]
-                            );
-                            \danog\MadelineProto\Logger::log($sentMessage);
-                        }
-                    } catch (Exception $e) {}
+                    if (!$silent) {
+                        $peer = $MadelineProto->get_info($update['update']['message']['to_id'])
+                        ['InputPeer'];
+                        $msg_id = $update['update']['message']['id'];
+                        $str = $MadelineProto->responses['bot_present']['not'];
+                        $repl = array(
+                            "botname" => getenv('BOT_USERNAME')
+                        );
+                        $message = $MadelineProto->engine->render($str, $repl);
+                        $sentMessage = $MadelineProto->messages->sendMessage(
+                            ['peer' => $peer, 'reply_to_msg_id' =>
+                            $msg_id, 'message' => $message]
+                        );
+                        \danog\MadelineProto\Logger::log($sentMessage);
+                    }
                     return false;
                 } else {
                     return true;
@@ -35,64 +33,61 @@ function bot_present($update, $MadelineProto, $silent = false, $peer = false, $u
             }
         }
         if (!$user) {
-            try {
-                $admins = cache_get_chat_info($update, $MadelineProto);
-                $peer = $admins['id'];
-                $bot_id = $MadelineProto->API->bot_id;
-                foreach ($admins['participants'] as $key) {
-                    if (array_key_exists('user', $key)) {
-                        $id = $key['user']['id'];
-                    } else {
-                        if (array_key_exists('bot', $key)) {
-                            $id = $key['bot']['id'];
-                        }
-                    }
-                    if ($id == $bot_id) {
-                        $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => true];
-                        return true;
+            $admins = cache_get_chat_info($update, $MadelineProto);
+            $peer = $admins['id'];
+            $bot_id = $MadelineProto->API->bot_id;
+            foreach ($admins['participants'] as $key) {
+                if (array_key_exists('user', $key)) {
+                    $id = $key['user']['id'];
+                } else {
+                    if (array_key_exists('bot', $key)) {
+                        $id = $key['bot']['id'];
                     }
                 }
-            } catch (Exception $e) {}
-        } else {
-            try {
-                $admins = cache_get_chat_info($update, $MadelineProto);
-                $peer = $admins['id'];
-                $bot_id = $MadelineProto->API->bot_api_id;
-                foreach ($admins['participants'] as $key) {
-                    if (array_key_exists('user', $key)) {
-                        $id = $key['user']['id'];
-                    } else {
-                        if (array_key_exists('bot', $key)) {
-                            $id = $key['bot']['id'];
-                        }
-                    }
-                    if ($id == $bot_id) {
-                        $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => true];
-                        return true;
-                    }
+                if ($id == $bot_id) {
+                    $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => true];
+                    return true;
                 }
-            } catch (Exception $e) {}
-        }
-        try {
-            if (!$silent) {
-                $peer = $MadelineProto->get_info($update['update']['message']['to_id'])
-                ['InputPeer'];
-                $msg_id = $update['update']['message']['id'];
-                $str = $MadelineProto->responses['bot_present']['not'];
-                $repl = array(
-                    "botname" => getenv('BOT_USERNAME')
-                );
-                $message = $MadelineProto->engine->render($str, $repl);
-                $sentMessage = $MadelineProto->messages->sendMessage(
-                    ['peer' => $peer, 'reply_to_msg_id' =>
-                    $msg_id, 'message' => $message]
-                );
-                \danog\MadelineProto\Logger::log($sentMessage);
             }
-        } catch (Exception $e) {}
+        } else {
+            $admins = cache_get_chat_info($update, $MadelineProto);
+            $peer = $admins['id'];
+            $bot_id = $MadelineProto->API->bot_api_id;
+            foreach ($admins['participants'] as $key) {
+                if (array_key_exists('user', $key)) {
+                    $id = $key['user']['id'];
+                } else {
+                    if (array_key_exists('bot', $key)) {
+                        $id = $key['bot']['id'];
+                    }
+                }
+                if ($id == $bot_id) {
+                    $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => true];
+                    return true;
+                }
+            }
+        }
+        if (!$silent) {
+            $peer = $MadelineProto->get_info($update['update']['message']['to_id'])
+            ['InputPeer'];
+            $msg_id = $update['update']['message']['id'];
+            $str = $MadelineProto->responses['bot_present']['not'];
+            $repl = array(
+                "botname" => getenv('BOT_USERNAME')
+            );
+            $message = $MadelineProto->engine->render($str, $repl);
+            $sentMessage = $MadelineProto->messages->sendMessage(
+                ['peer' => $peer, 'reply_to_msg_id' =>
+                $msg_id, 'message' => $message]
+            );
+            \danog\MadelineProto\Logger::log($sentMessage);
+        }
         $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => false];
         return false;
-    } catch (Exception $e) {}
+    } catch (Exception $e) {
+        $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => false];
+        return false;
+    }
     $MadelineProto->API->is_bot_present[$peer] = ["timestamp" => time(), "return" => false];
     return false;
 }
